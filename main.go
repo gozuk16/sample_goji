5cb30d683576c1939ee49ad7523bac70e08cb47a
GOZU Kenichiro <gozuk16@gmail.com>
2021-05-29 16:20:46 +0900

    helloというAPIとstaticを作成

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/zenazn/goji"
+	"github.com/zenazn/goji/web"
+)
+
+func hello(c web.C, w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintf(w, "Hello, %s!", c.URLParams["name"])
+	w.Header().Set("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	result := fmt.Sprintf("Hello, %s!", c.URLParams["name"])
+	encoder.Encode(result)
+}
+
+func main() {
+	goji.Get("/hello/:name", hello)
+	goji.Get("/docs/*", http.FileServer(http.Dir(".")))
+	goji.Serve()
+}
